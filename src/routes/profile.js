0bf4bd4f39bcd4330b1b0e3a9d7546af64b2ffbe
alihamza-12 <alihamzaarshad12@gmail.com>
2026-01-07 17:18:57 +0500

    edit profile & validate that data from helper func

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { userAuth } = require("../middlewares/userAuth");
+const { validateEditProfileData } = require("../utils/validation");
 
 const profileRoute = express.Router();
 
@@ -9,6 +10,29 @@ profileRoute.get("/profile/view", userAuth, async (req, res) => {
     //recived from the middleware(userAuth.js)
     const findUser = req.findUser;
     res.send(findUser);
+  } catch (error) {
+    res.status(400).send("ERROR : " + error.message);
+  }
+});
+//Edit the user profile
+profileRoute.patch("/profile/edit", userAuth, async (req, res) => {
+  try {
+    //helper Function
+    if (!validateEditProfileData(req)) {
+      throw new Error("Invalid Data for Edit");
+    }
+    //recived from the middleware(userAuth.js)
+    const loggedInUser = req.findUser;
+    //updating the data of loggedinuser
+    // loggedInUser.firstName=req.body.firstName
+    Object.keys(req.body).forEach((key) => (loggedInUser[key] = req.body[key]));
+    //save the loggedinuser to database
+    await loggedInUser.save();
+
+    res.json({
+      message: `${loggedInUser.firstName} , your profile is Updated`,
+      data: loggedInUser,
+    });
   } catch (error) {
     res.status(400).send("ERROR : " + error.message);
   }
--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,8 +1,9 @@
 const validator = require("validator");
 
+//validating user input data
 const validatorForSignUp = (req) => {
   //Getting data of User from the req.body
-  const { firstName, lastName, email, password, gender, skills, age } = req.body;
+  const { firstName, lastName, email, password, age } = req.body;
 
   //Validation of data
   //Name
@@ -27,11 +28,47 @@ const validatorForSignUp = (req) => {
     throw new Error("Age must be greater then 18");
   }
   //Skills Must be 10 only
-//   else if (skills.length !== 10) {
-//     throw new Error("Skills Must be 10 only");
-//   }
+  //   else if (skills.length !== 10) {
+  //     throw new Error("Skills Must be 10 only");
+  //   }
 };
 
-module.exports={
-    validatorForSignUp,
-}
+//patch request helper function
+const validateEditProfileData = (req) => {
+  const { firstName, lastName, skills, gender, age } = req.body;
+
+  //validation of data
+  if (skills.length > 10) {
+    throw new Error("Skills cannot be more then 10");
+  } else if (!firstName || !lastName) {
+    throw new Error("Name is Required");
+  }
+  //Length of Name
+  else if (firstName.length > 10 || firstName.length < 3) {
+    throw new Error("Fisrt Name length must be 3 to 10 Letters");
+  } else if (lastName.length > 10 || lastName.length < 3) {
+    throw new Error("Last Name length must be 3 to 10 Letters");
+  }
+  //gender
+  else if (!age || age < 18) {
+    throw new Error("Age must be greater then 18");
+  }
+
+  const allowEditFields = [
+    "firstName",
+    "lastName",
+    "photoUrl",
+    "gender",
+    "skills",
+    "age",
+  ];
+  const isEditAllowed = Object.keys(req.body).every((field) =>
+    allowEditFields.includes(field)
+  );
+  return isEditAllowed;
+};
+
+module.exports = {
+  validatorForSignUp,
+  validateEditProfileData,
+};
